ae5e45fde52013ab2271cb0bdeffb59d5b1701ee
d1_codes <62024664+anekemichael@users.noreply.github.com>
2022-06-24 01:39:52 +0100

    Updated

---
+++ Zuriapp.js
@@ -0,0 +1,9 @@
+const myName = 'Name: Aneke Michael,'
+const height = ' Height: 6.2ft,'
+const country = ' Country: Nigeria.'
+
+window.alert(myName + height + country);
+
+document.write(myName);
+document.write(height);
+document.write(country);
